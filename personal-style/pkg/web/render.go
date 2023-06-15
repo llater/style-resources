4bfb3af0e25d8d144752d750d9807e6b6bdc06e9
MacBook <macbook@ir0.lelandlater.net>
2023-06-15 11:20:22 -0700

    Format

--- personal-style/pkg/web/render.go
+++ personal-style/pkg/web/render.go
@@ -1,23 +1,24 @@
 package web
 
 import (
-  "html/template"
-  "os"
+	"html/template"
+	"os"
 )
 
 type CssTemplate struct {
-  Colors struct {
-    Primary string
-    Secondary string
-    Background string
-  }   
+	Colors struct {
+		Primary    string
+		Secondary  string
+		Background string
+	}
 }
 
 func NewCssTemplate() *CssTemplate {
-  return &CssTemplate{
-    Colors: {
-      Primary: "cyan",
-      Secondary: "hotpink",
-      Background: "gray",
-    },
+	return &CssTemplate{
+		Colors: {
+			Primary:    "cyan",
+			Secondary:  "hotpink",
+			Background: "gray",
+		},
+	}
 }
